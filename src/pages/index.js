fe18c7e354f95fc128664d4cf105c7b6e89510f9
Theo Stanton <theo@Theos-MBP.fritz.box>
2018-09-30 20:24:30 +0100

    Remove css from index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,12 @@
 import React from 'react'
 import { Link, graphql } from 'gatsby'
-import { css } from 'react-emotion'
-import styles from './index.modules.css'
 import Layout from '../components/layout'
 
-console.log(styles)
 
 const Job = props => (
-  <div className={styles.job} key={props.node.id}>
-    <Link
-      to={props.node.fields.slug}
-      className={css`
-                text-decoration: none;
-                color: inherit;
-              `
-      }
-    >
-      <h3>
-        {props.node.frontmatter.title}{' '}
-        <span
-          className={css`
-                  color: #bbb;
-                `}
-        >
-                — {props.node.frontmatter.subtitle}
-              </span>
-      </h3>
+  <div key={props.node.id}>
+    <Link to={props.node.fields.slug}>
+      <h3>{props.node.frontmatter.title}{' '} <span>— {props.node.frontmatter.subtitle}</span></h3>
       <p>{props.node.frontmatter.description}</p>
     </Link>
   </div>
