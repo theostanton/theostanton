78854fd155746c4996305c8fa9ed1d016dc57cd1
Theo Stanton <theo@Theos-MBP.fritz.box>
2018-10-02 21:50:53 +0100

    push

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,11 @@ import { Link, graphql } from 'gatsby'
 import Layout from '../components/layout'
 import style from './index.module.css'
 
+console.log(style)
 
 const Job = props => (
-  <Link to={props.node.fields.slug}>
-    <div className={style.job} key={props.node.id}>
+  <Link to={props.node.fields.slug} key={props.node.id}>
+    <div className={style.job}>
       <h3>{props.node.frontmatter.title}</h3>
       <h5>{props.node.frontmatter.subtitle}</h5>
       {props.node.frontmatter.description}
