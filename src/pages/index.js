c0f33f1f02f7090cf4ab98cd9b530925a106d6d7
Theo Stanton <theo@Theos-MBP.fritz.box>
2018-10-09 21:26:50 +0100

    push

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,9 @@ console.log(style)
 const Job = props => (
   <Link to={props.node.fields.slug} key={props.node.id}>
     <div className={style.job}>
-      <h3>{props.node.frontmatter.title}</h3>
-      <h5>{props.node.frontmatter.subtitle}</h5>
-      {props.node.frontmatter.description}
+      <h3 className={style.text}>{props.node.frontmatter.title}</h3>
+      <h5 className={style.text}>{props.node.frontmatter.subtitle}</h5>
+      <p className={style.description}>{props.node.frontmatter.description}</p>
     </div>
   </Link>
 )
