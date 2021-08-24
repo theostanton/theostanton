e8256373c3feb24e1b3074ad8a30935d818f2997
Theo <theo@theo.dev>
2021-08-24 13:42:03 +0200

    db.t3.micro

--- deploy/instance/database.tf
+++ deploy/instance/database.tf
@@ -5,7 +5,7 @@ resource "random_password" "database" {
 
 resource "aws_db_instance" "database" {
   identifier = "theodev${local.domain_suffix}"
-  instance_class = "db.t2.micro"
+  instance_class = "db.t3.micro"
   apply_immediately = true
   allocated_storage = 10
   engine = "postgres"
